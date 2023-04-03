3f896a5ee29995098ad3fd1965cb8952d3976654
Rohit Jha <jharohit25@gmail.com>
2023-04-03 16:51:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from './Components/Posts';
+import PostForm from './Components/PostForm';
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm/>
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
